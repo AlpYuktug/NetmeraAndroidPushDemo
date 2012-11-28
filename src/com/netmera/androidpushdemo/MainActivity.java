3b79994b4ed72661bee1d196db5de10f09bfc5f5
Falko Richter <falko.richter@sinnerschrader-mobile.de>
2012-11-28 21:14:01 +0100

    added a button to send a push from a mobile as well

--- src/com/netmera/androidpushdemo/MainActivity.java
+++ src/com/netmera/androidpushdemo/MainActivity.java
@@ -1,11 +1,19 @@
 package com.netmera.androidpushdemo;
 
+import java.util.Map;
+
 import com.netmera.androidpushdemo.R;
+import com.netmera.mobile.BasePush.PushChannel;
 import com.netmera.mobile.NetmeraClient;
+import com.netmera.mobile.NetmeraException;
+import com.netmera.mobile.NetmeraPush;
+import com.netmera.mobile.NetmeraPushDetail;
 import com.netmera.mobile.NetmeraPushService;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Looper;
 import android.text.Html;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +25,7 @@ import android.view.View.OnClickListener;
 public class MainActivity extends Activity {
 
 	private LinearLayout pushLayout;
-	
+
 	private TextView registerIdTextView;		// To show the reg. ID
 	private TextView registerStatusTextView;	// To show the reg. status
 
@@ -29,12 +37,14 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_page);
 		pushLayout = (LinearLayout) findViewById(R.id.mainLayout);
-		
+
 		// Add status text views to the layout
 		registerIdTextView = new TextView(this);
 		pushLayout.addView(registerIdTextView);
 		registerStatusTextView = new TextView(this);
 		pushLayout.addView(registerStatusTextView);
+
+
 
 		// Calling init() method with api key to be able to use netmera services.
 		NetmeraClient.init(this, GlobalVariables.apiKey);
@@ -71,7 +81,11 @@ public class MainActivity extends Activity {
 					 * activityClass : This is the activity launched when user clickes on the notification.
 					 * projectID : This is the ID that you get from the Google on the step-1.
 					 */
-					NetmeraPushService.register(getApplicationContext(), PushActivity.class, GlobalVariables.projectId);
+					try {
+						NetmeraPushService.register(getApplicationContext(), PushActivity.class, GlobalVariables.projectId);
+					} catch (NetmeraException e) {
+						e.printStackTrace();
+					}
 				}
 			}
 		});
@@ -88,6 +102,45 @@ public class MainActivity extends Activity {
 		});
 		pushLayout.addView(refreshButton);
 
+		Button sendPushButton = new Button(this);
+		sendPushButton.setText("send hello world push");
+		sendPushButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				
+				Thread thread = new Thread(new Runnable() {
+					
+					@Override
+					public void run() {
+						Looper.prepare();
+						NetmeraPush push = new NetmeraPush();
+
+						// Send to android devices
+						push.setSendToAndroid(true);
+						push.setSendToIos(true);
+						try {
+							// Set the message you want to send 
+							// to registered devices in your app.
+							push.setMessage("Hello, World!");
+							// Send the notification and get the result as a Map
+							Map<PushChannel, NetmeraPushDetail> result = push.sendNotification();
+							
+							// Get the push detail of channel and then get the status, error, message etc...
+							NetmeraPushDetail androidDetail = result.get(PushChannel.android);
+							String androidStatus = androidDetail.getStatus();
+							Toast.makeText(MainActivity.this, "android:"+androidStatus, Toast.LENGTH_SHORT).show();
+
+						} catch (NetmeraException e) {
+							e.printStackTrace();
+						}
+						
+					}
+				});
+				thread.start();
+			}
+		});
+		pushLayout.addView(sendPushButton);
 		showPage();
 	}
 
